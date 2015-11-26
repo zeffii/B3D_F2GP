4bb14fd6a6a16dcb4c94df4699b313f963f3db41
Dealga McArdle <tetha.z@gmail.com>
2015-11-26 15:44:57 +0100

    small steps towards panel

--- __init__.py
+++ __init__.py
@@ -1,14 +1,29 @@
 import bpy
+from bpy.props import StringProperty
 
-def get_layer(gdata_owner, layer_name):
 
-    grease_data = bpy.data.grease_pencil
-    if gdata_owner not in grease_data:
-        gp = grease_data.new(gdata_owner)
+bl_info = {
+    "name": "Font to GP",
+    "author": "Dealga McArdle",
+    "version": (0, 1),
+    "blender": (2, 7, 6),
+    "location": "",
+    "description": "",
+    "warning": "",
+    "wiki_url": "",
+    "tracker_url": "",
+    "category": "3D View"
+}
+
+
+def get_layer(GP, gdata_owner, layer_name):
+
+    if gdata_owner not in GP:
+        gp = GP.new(gdata_owner)
     else:
-        gp = grease_data[gdata_owner]
+        gp = GP[gdata_owner]
 
-    # get grease pencil layer
+    # create new or pick up named GP layer
     if not (layer_name in gp.layers):
         layer = gp.layers.new(layer_name)
         layer.frames.new(1)
@@ -19,13 +34,13 @@ def get_layer(gdata_owner, layer_name):
 
     return layer
 
-def generate_gp3d_stroke(obj, layer):
+
+def generate_gp3d_stroke(mesh, layer):
     layer.show_points = True
     layer.color = (0.2, 0.90, .2)
 
-    verts = obj.data.vertices
-    for f in obj.data.polygons:
-
+    verts = mesh.vertices
+    for f in mesh.polygons:
         s = layer.frames[0].strokes.new()
         s.draw_mode = '3DSPACE'  # or '2DSPACE'
         s.points.add(len(f.vertices))
@@ -34,11 +49,55 @@ def generate_gp3d_stroke(obj, layer):
             p.co = verts[v].co
             p.pressure = 1.0
 
-def main():
-    obj = bpy.context.active_object
-    data_name, layer_name = 'stack_data', "stack layer"
-    layer = get_layer(data_name, layer_name)
-    generate_gp3d_stroke(obj, layer)
-    bpy.context.scene.grease_pencil = bpy.data.grease_pencil[data_name]
 
-main()
+class F2GPDispatcher(bpy.types.Operator):
+
+    bl_label = "Short Name"
+    bl_idname = "wm.f2gp_callback"
+
+    fn_name = StringProperty(default='')
+    data_name = StringProperty(default='stack_data')
+    layer_name = StringProperty(default='stack_layer')
+
+    def execute(self, context):
+        if self.fn_name == 'set_gp_from_font':
+            GP = bpy.data.grease_pencil
+            layer = get_layer(self.data_name, self.layer_name)
+            generate_gp3d_stroke(GP, mesh, layer)
+            bpy.context.scene.grease_pencil = GP[self.data_name]
+        return {'FINISHED'}
+
+
+class F2GPCommandPanel(bpy.types.Panel):
+
+    bl_label = "Convert Font To GP"
+    bl_idname = "OBJECT_PT_f2gp"
+    bl_space_type = '3DVIEW'
+    bl_region_type = 'TOOL_PROPS'
+
+    def draw(self, context):
+        scn = context.scene
+        layout = self.layout
+
+        row = layout.row()
+        row.prop(scn, 'f2gp_data_name')
+        row.prop(scn, 'f2gp_layer_name')
+
+        row = layout.row()
+        maker = row.operator("mesh.primitive_cube_add", text='make gp')
+        maker.data_name = scn.f2gp_data_name
+        maker.layer_name = scn.f2gp_layer_name
+
+
+def register():
+    scn = bpy.types.Scene
+    scn.f2gp_data_name = StringProperty(default='stack_data')
+    scn.f2gp_layer_name = StringProperty(default='stack_layer')
+    bpy.utils.register_module(__name__)
+
+
+def unregister():
+    bpy.utils.unregister_module(__name__)
+    scn = bpy.types.Scene
+    del scn.f2gp_data_name
+    del scn.f2gp_layer_name
